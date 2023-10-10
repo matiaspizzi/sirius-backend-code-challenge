42c7dd01897943595a4e64fd0f6671941e939701
ron2452 <87399661+ron2452@users.noreply.github.com>
2023-10-10 10:41:42 -0300

    ref

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
 import dotenv from 'dotenv';
 import express from 'express';
-import { adminAuth, userAuth } from './middlewares/auth.middlewares';
-import userRoute from './routes/user.routes';
-import mailRoutes from './routes/mail.routes';
-import adminRoute from './routes/admin.routes';
 import { PrismaClient } from '@prisma/client';
 
 dotenv.config();
@@ -11,11 +7,6 @@ const app = express();
 let db_url = process.env['DATABASE_URL'] as string;
 if (process.env['NODE_ENV'] === 'test') db_url = process.env['DATABASE_URL_TEST'] as string;
 const prisma = new PrismaClient({ datasources: { db: { url: db_url } } });
-
-app.use(express.json());
-app.use('/', userRoute);
-app.use('/', userAuth, mailRoutes);
-app.use('/', adminAuth, adminRoute);
 
 const PORT = process.env['PORT'] || 3000;
 const server = app
